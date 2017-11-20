0c7a5a0393e206960eb5332147e6705060b3bb69
svlentink <sv@lent.ink>
2017-11-20 17:37:53 +0100

    raw untested code

--- controller/main.py
+++ controller/main.py
@@ -3,15 +3,71 @@
 import subprocess
 from flask import Flask
 from flask import request
+import requests
+import time
+from threading import Thread
 app = Flask(__name__)
 
+from scenarios import *
 
-@app.route('/GET/docker/cli', methods=['GET'])
-def get_docker_cli():
-  print(request)
-  cmd = "docker run -it --rm alpine echo TODO"
+
+@app.route('/GET/<cont>/<scenario>/<int:inp>', methods=['GET'])
+def get_docker_cli(cont, scenario, inp):
+  print(request) # debug
+  if cont not in container_technologies:
+    print('Unknown container technology, please use one of', container_technologies)
+    return('Unknown container technology',500)
+  if scenario not in scenarios:
+    print('Unknown scenario, pleas use one of', scenarios)
+    return('Unknown scenario',500)
+  
+  cmds = scenarios[scenario][cont]
+  if 'port' in scenarios[scenario]:
+    # type is server
+    port = scenarios[scenarios]['port']
+    result = get_response_server(cmds,inp)
+  else:
+    # type is cli
+    result = get_response_cli(cmds,inp)
+  return(result,200)
+
+
+def get_response_server(cmds, inp, port):
+  subprocess.call(cmds['init'], shell=True)
+  delayed_kill(cmds['kill'])
+  url = 'localhost:' + port + '/' + inp
+  return requests.get(url).json
+
+
+def get_response_cli(cmd,inp):
+  '''
+  We perform the given command, the input is given to the CLI application
+  '''
+  result = subprocess.check_output(cmd + ' ' + inp, shell=True)
+  return result
+
+
+def delayed_cmd(cmd):
+  time.sleep(3)
   subprocess.call(cmd, shell=True)
-  return('Starting',200)
+
+def delayed_kill(cmd):
+  '''
+  We can define a web request as one request,
+  which will load multiple sources (e.g. css, html, js)
+  for this reason, we want our container to keep running
+  for a short time, to be sure that it can respond to multiple
+  http request for a single web page request.
+  
+  The delayed kill time should be rescheduled every time a container is triggered.
+  However, for this research, we generate a single JSON and do not
+  keep track if something is running and use a fixed time it is killed.
+  This makes it NOT suited for any real applications,
+  since we act upon this limitation by scheduling our requests.
+  '''
+  bg_thread = Thread(target=delayed_cmd, args=(cmd,))
+  bg_thread.start()
+
 
 if __name__ == '__main__':
   app.run(debug=True, port=8080, host='0.0.0.0')
---
+++ controller/scenarios.py
@@ -0,0 +1,27 @@
+
+container_technologies = ['docker','rkt','lxc','openvz']
+
+# These scenarios define what commands will be run
+# you can have a cli based application,
+# will is just a string which we run, the input (a number)
+# will be appended to it.
+# Another scenario is the server applications
+# these require a port on which they will listen
+# a init commond, to get the server running
+# and a kill command, to terminate the server.
+scenarios = {
+  'lamp-server':{
+    'docker':{
+      'init' : 'docker run -p 8888:80 bla',
+      'kill' : 'docker stop bla'
+    },
+    'rkt':{
+      'init': '',
+      ''
+    },
+    'port' : 8888 # this means that is is a running server
+  },
+  'bash-cli':{},
+  'python-cli':{},
+  'nodejs-cli':{}
+}
