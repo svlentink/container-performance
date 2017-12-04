a273d2a365bd70734786d38ad3dcb8b13965a54c
svlentink <sv@lent.ink>
2017-12-04 14:30:50 +0100

    extra comments

--- controller/gather-metrics.py
+++ controller/gather-metrics.py
@@ -12,12 +12,14 @@ amount_of_tests_per_case = 1000
 sleep_interval_between_tests = 3 #sec
 data_output_path = '/root'
 
+
 def measure_millis(url):
   startt = int(time.time() * 1000)
   r = requests.get(url)
   stopt = int(time.time() * 1000)
   delta = stopt - startt
   return { 'd' : delta, 's' : r.status_code }
+
 
 def gather_metrics(conttype):
   results = {}
@@ -29,7 +31,9 @@ def gather_metrics(conttype):
       url = 'http://127.0.0.1:8081/GET/' + reqtype + '/' + platform + '/' + conttype + '/' + str(nr)
       
       for i in range(10):
-        #first 10 runs, for filecaching etc.
+        # To allow system optimizations (e.g. caching)
+        # and initial container startup latency
+        # we first start the container 10 times
         measure_millis(url)
       
       arr = []
@@ -42,9 +46,6 @@ def gather_metrics(conttype):
       results[reqtype][platform] = arr
       with open(filename, 'w') as f:
         json.dump(results,f,default=str)
-
-
-
 
 
 
