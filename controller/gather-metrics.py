8dc06d5fd9efe95ff4a281559ec89e3c0e4437d4
svlentink <sv@lent.ink>
2017-12-06 17:57:51 +0100

    different sleep intervals for server and cli

--- controller/gather-metrics.py
+++ controller/gather-metrics.py
@@ -7,10 +7,17 @@ from random import randrange
 import json
 import time
 
+
+
 print('This script is the heart of the research, it gathers the numbers.')
-amount_of_tests_per_case = 10
-sleep_interval_between_tests = 10 #sec
-data_output_path = '/root'
+amount_of_tests_per_case = 100
+sleep_interval_between_tests = {
+  "cli" : 2, #sec
+  'server' : 15 #sec
+}
+testdata_output_path = '/root'
+
+
 
 
 def measure_millis(url):
@@ -23,7 +30,7 @@ def measure_millis(url):
 
 def gather_metrics(conttype):
   results = {}
-  filename = data_output_path + '/' + conttype + str(int(time.time())) + '.json'
+  filename = testdata_output_path + '/' + conttype + str(int(time.time())) + '.json'
   for reqtype in scenarios:
     results[reqtype] = {}
     for platform in scenarios[reqtype]:
@@ -34,12 +41,12 @@ def gather_metrics(conttype):
         # To allow system optimizations (e.g. caching)
         # and initial container startup latency
         # we first start the container 10 times
-        time.sleep(sleep_interval_between_tests)
+        time.sleep(sleep_interval_between_tests[reqtype])
         measure_millis(url)
       
       arr = []
       for i in range(amount_of_tests_per_case):
-        time.sleep(sleep_interval_between_tests)
+        time.sleep(sleep_interval_between_tests[reqtype])
         metric = measure_millis(url + str(i))
         arr.append(metric)
       
